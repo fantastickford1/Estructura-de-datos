f9d6e4f6aaf57a581cac760983d7f12aa0a5c013
fantastickford1 <carlitos_zenteno@hotmail.com>
2015-09-07 20:28:21 -0500

    Stock Validation Final
    Final version (Ready for delivery)

--- Stock/StockValidation.java
+++ Stock/StockValidation.java
@@ -1,13 +1,29 @@
 public class StockValidation{
 
+  private String formula;
+  private char[] newform;
 
-  public StockValidation(PilaChar pila2, int length){
-
+  public StockValidation(String formula, char[] newform){
+    this.formula = formula;
+    this.newform = newform;
   }
 
-  public void validateF(){
-    pila2.push('2');
-    System.out.println(pila2.toString());
+  public boolean validate(PilaChar pila){
+    for (int i = 0;i<formula.length() ; i++ ) {
+      if(newform[i] == '('){
+        pila.push(newform[i]);
+      }else if (newform[i] == ')') {
+        if (!pila.isEmpty()) {
+          pila.pop();
+        }else {
+          return false;
+        }
+      }
+    }
+    if (pila.isEmpty()){
+      return true;
+    }else
+      return false;
   }
 
 }
--- Stock/TestM.java
+++ Stock/TestM.java
@@ -4,24 +4,15 @@ import java.util.Scanner;
 public class TestM{
   public static void main(String[] args) {
 
+    StockValidation skvalidation;
     Scanner keyboard = new Scanner(System.in);
     System.out.println("Introduce la formula");
     String formula = keyboard.nextLine();
     char[] newform = formula.toCharArray();
     PilaChar pila = new PilaChar(formula.length());
-    for (int i = 0;i<formula.length() ; i++ ) {
-      if(newform[i] == '('){
-        pila.push(newform[i]);
-      }else if (newform[i] == ')') {
-        if (!pila.isEmpty()) {
-          pila.pop();
-        }else {
-          pila.push(')');
-        }
-      }
-    }
-    if (pila.isEmpty()){
-      System.out.println("Formato correcto");
+    skvalidation = new StockValidation(formula,newform);
+    if(skvalidation.validate(pila) == true){
+        System.out.println("Formato correcto");
     }else
       System.out.println("Formato incorrecto");
 
