b8218c6edad1f3b070e46c91838b728ad55dada5
fantastickford1 <carlitos_zenteno@hotmail.com>
2015-10-04 20:38:50 -0500

    ListaSimpleTest FinalVersion
    Complete

--- Test/SimpleListTest.java
+++ Test/SimpleListTest.java
@@ -29,12 +29,25 @@ public class SimpleListTest{
   public void test_getPosition(){
     System.out.println("Validad get()");
     ListaSimple list = new ListaSimple();
-    list.add();
-    list.add();
-    list.add();
-    list.add();
-    list.add();
-    System.out.println("::::::" + );
+    list.add("Tere");
+    list.add("Nacho");
+    list.add("Diana");
+    list.add("Carlos");
+    list.add("Karen");
+    System.out.println("::::::" + list.get(3).getData() );
+  }
+
+  @Test
+  public void test_toString(){
+    System.out.println("Validar toString()");
+    ListaSimple list = new ListaSimple();
+    list.add("Diana");
+    list.add("Carlos");
+    list.add("Tere");
+    list.add("Nacho");
+    list.add("Karen");
+    list.add("Susi");
+    System.out.println("::::::" + list.toString());
   }
 
 
